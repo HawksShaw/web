567677a3b4c7eb272d02ce0638dcbee551fd2af8
Robert Wyrobek <robik272003@gmail.com>
2026-06-17 14:31:21 +0200

    2 Lab zrobiony

--- fizjo/fizjoapp/urls.py
+++ fizjo/fizjoapp/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('dodaj-fizjo/', views.add_fizjo, name='dodaj_fizjoterapeute'),
-    path('dodaj-pacjenta/', views.add_pacjent, name='dodaj pacjenta'),
+    path('dodaj-pacjenta/', views.add_pacjent, name='dodaj_pacjenta'),
     path('dodaj-program/', views.add_program, name='dodaj_program'),
 ]
